825781b430465b2c3a6c3eb805c7509dea6e73e7
jackie trillo <jackietrillo@hotmail.com>
2022-07-09 18:03:05 -0700

    moved images folder

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,9 @@
-import logo from "./images/logo.png";
 import NavBar from "./NavBar";
 
 function Header() {
   return (
     <header className="header shadow-sm p-3 mb-5">
-      <img src={logo} className="app-logo" />
+      <img src="images/logo.png" className="app-logo" />
       <NavBar />
     </header>
   );
