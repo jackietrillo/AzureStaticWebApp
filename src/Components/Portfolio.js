2e616610f9f24aedc4dd57b43470083f98f4e7ab
jackie trillo <jackietrillo@hotmail.com>
2022-07-17 20:38:50 -0700

    changes to header and menu

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,20 @@
+import Header from "../Header";
+import Footer from "../Footer";
+
+const Portfolio = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h2 className="section-title">
+          <span>Portfolio</span>
+        </h2>
+        <div className="spacer"></div>
+        <p>Coming soon!</p>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Portfolio;
---
+++ src/Components/Resume.js
@@ -0,0 +1,20 @@
+import Header from "../Header";
+import Footer from "../Footer";
+
+const Resume = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h2 className="section-title">
+          <span>Experience</span>
+        </h2>
+        <div className="spacer"></div>
+        <p>Coming soon!</p>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Resume;
